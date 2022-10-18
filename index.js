e38d307e79da16f8f209a44858210a182532d8a4
Tyler Pastor <tpast001@ucr.edu>
2022-10-17 17:12:37 -0700

    Updating changes

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
+const dotenv = require('dotenv');
 const port = process.env.port || 3000;
+
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -22,12 +25,48 @@ app.post("/widget_api/definition", cors(), (req, res) => {
     };
     
     async function getData (){
+        var termList = [];
+        var data = [];
+
+        //Parse User Text
+        var userText = req.body.text.replace(/ /g, '+');
+
+        //Call the tagger API and get the terms
+        var res = await axios.get("http://data.bioontology.org/annotator?text=" + userText + "&longest_only=true&apikey=" + process.env.API_KEY);
+        var terms = res.data;
+        
+        for(let i = 0; i < terms.length; i++){
+            if(termList.indexOf(terms[i].annotations[0].text) == -1){
+                termList.push(terms[i].annotations[0].text);
+            }
+        }
+
+        console.log(termList);
+        
+        for(let i = 0; i < termList.length; i++){
+            var res = await axios.get("http://localhost:5000/definition/term/" + termList[i]);
+            data.push({[termList[i]]: res.data});
+        }
+
+        return callBack(data);
+    }
+
+    getData();
+});
+
+//Relations
+app.post("/widget_api/relation", cors(), (req, res) => {
+    function callBack(data){
+        res.json(data); 
+    };
+    
+    async function getData (){
         //Call the tagger API and get the terms
         var terms = ["heart", "kidney", "muscle", "cancer"];
         var data = [];
         
         for(let i = 0; i < terms.length; i++){
-            var res = await axios.get("http://localhost:5000/definition/term/" + terms[i]);
+            var res = await axios.get("http://localhost:5000/relation/term/" + terms[i]);
             data.push({[terms[i]]: res.data});
         }
 
